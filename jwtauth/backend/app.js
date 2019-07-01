b4491fbc8891d1332ab159ab1124041e51c175e3
kbarnes775 <kylebarnes7@gmail.com>
2019-07-01 11:39:49 -0700

    Added Frontend

--- jwtauth/backend/app.js
+++ jwtauth/backend/app.js
@@ -28,4 +28,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
-});});
+});
--- jwtauth/backend/db.js
+++ jwtauth/backend/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/auth'
+    DB: 'mongodb+srv://admin:webdesigns@cluster0-ffc01.mongodb.net/test?retryWrites=true&w=majority'
 }
