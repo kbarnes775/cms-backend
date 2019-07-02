c76eb4a4ddae5f82809ca67fe3fe468ffa2ce583
kbarnes775 <kylebarnes7@gmail.com>
2019-07-02 09:25:58 -0700

    Fixed Validation
    Fixed Validation and added user name to navbar when logged in

--- jwtauth/frontend/src/components/Home.js
+++ jwtauth/frontend/src/components/Home.js
@@ -4,7 +4,8 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                Home Component
+                <p>Welcome to Webspot 2.0. </p>
+                <p>A custom CMS made with React, MongoDB, Express JS, and Node.js.</p>
             </div>
         );
     }
